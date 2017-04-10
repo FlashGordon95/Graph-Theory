8d5dcd794ed2e2589a422c5b54f2dfdc09298329
FlashGordon95 <ryangordon210@gmail.com>
2017-04-10 21:53:23 +0100

    Import page as DOM Tree and extract the room data.
    Doing this using the etree function of lxml.
    Steps:
    Downloaded the webpage as a webarchive
    Load it in using etree.HTMLParser
    Locate the select element with path
    Strip out the text for each element and place in array.

--- Project/Web-Scraper/gmitRooms.py
+++ Project/Web-Scraper/gmitRooms.py
@@ -1,2 +1,10 @@
 from lxml import etree
 import csv
+rooms = []
+hparser = etree.HTMLParser(encoding='utf-8')
+tree   = etree.parse('gmit-rooms-doc.webarchive', hparser)
+options = tree.xpath("//select[@name='dlObject']/option")
+rooms = [option.text for option in options]
+print(rooms)
+out = csv.writer(open("myfile.csv","w"), delimiter=',',quoting=csv.QUOTE_ALL)
+
